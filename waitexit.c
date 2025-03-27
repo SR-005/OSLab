14e9ac44b70bf1d27ddb18269e97255e0615d62b
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-03-27 22:40:34 +0530

    update

---
+++ waitexit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+int main()
+{
+    pid_t cpid;
+    if(fork()==0)
+    {
+        exit(0);
+    }
+    else
+    {
+        cpid=wait(NULL);
+        printf("Parent PID: %d\n", getpid());
+        printf("Child PID: %d\n", cpid)
+    }
+    return 0;
+}
